3d20ecc827d05fc221888066a8fe8328e5f34e43
kauasilval <kaua60033@gmail.com>
2024-09-20 07:43:16 -0300

    teste

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if (isset($_POST['Entrar'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="quizz/css/login.css">
-    <title>Ludiflex | Login & Registration</title>
+    <title>Ludiflex | Login & Registrationn</title>
 </head>
 
 <body>
