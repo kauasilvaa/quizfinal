ea7147f3f1f85453b073d48c26d4235a0b6cebbe
kauasilval <kaua60033@gmail.com>
2024-10-09 14:59:17 -0300

    db

--- quizz/db/js/quiz (6).sql
+++ quizz/db/js/quiz (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Set-2024 às 19:43
+-- Tempo de geração: 09-Out-2024 às 19:57
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -34,22 +34,17 @@ CREATE TABLE `perguntas` (
   `opcao2` varchar(255) NOT NULL,
   `opcao3` varchar(255) NOT NULL,
   `opcao4` varchar(255) NOT NULL,
-  `resposta_correta` tinyint(1) NOT NULL
+  `resposta_correta` tinyint(1) NOT NULL,
+  `conclusao` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Extraindo dados da tabela `perguntas`
 --
 
-INSERT INTO `perguntas` (`id_perguntas`, `titulo_pergunta`, `opcao1`, `opcao2`, `opcao3`, `opcao4`, `resposta_correta`) VALUES
-(1, 'a', 'a', 'a', 'a', 'a', 1),
-(2, 'a', 'a', 'a', 'a', 'a', 1),
-(3, 'a', 'a', 'a', 'a', 'a', 1),
-(4, 'a', 'a', 'a', 'a', 'a', 1),
-(5, 'a', 'a', 'a', 'a', 'a', 1),
-(15, 'rere', 'sasas', 'rerer', 'erer', 'asasa', 2),
-(16, 'rere', 'sasas', 'rerer', 'erer', 'asasa', 2),
-(17, 'Batata', 'aaaaaaaaaa', 'rerer', 'erer', 'aaaaaaaaaa', 2);
+INSERT INTO `perguntas` (`id_perguntas`, `titulo_pergunta`, `opcao1`, `opcao2`, `opcao3`, `opcao4`, `resposta_correta`, `conclusao`) VALUES
+(32, 'tvtv', 'popo', 'popo', 'popo', 'popo', 4, 'verver'),
+(33, 'Batata', 'aaaaaaaaaa', 'rerer', 'papa', 'tetete', 3, 'pqpqppq');
 
 -- --------------------------------------------------------
 
@@ -71,10 +66,17 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nome`, `nomedeusuario`, `email`, `senha`, `nivel_acesso`) VALUES
-(1, 'caio', 'cdd', 'jfuyf@fuf', 'khjlçkh', 0),
 (2, 'Kauã', 'kaua', 'kaua60033@gmail.com', 'kaua123', 1),
 (5, 'dudao', 'dudao', 'mateusoliiveira000@gmail.com', 'duda123', 0),
-(7, 'carlao', 'carlao', 'carlaos@gmail.com', 'carlos123', 0);
+(7, 'carlao', 'carlao', 'carlaos@gmail.com', 'carlos123', 0),
+(8, 'caio', 'caioo', 'carlaos@gmail.com', 'caio123', 0),
+(9, 'Ruan', 'Ruan123', 'juan.juan@portalsesisp.org.br', '12345', 0),
+(10, 'angelo', 'angelo ostroski', 'angeloo@portalsesisp.org.br', 'troski6608', 0),
+(11, 'Filipe e Anna ', 'FilipeeAnna', 'filipeanna123@gmail.com', 'filipeanna123', 0),
+(12, 'filipeanna', 'filipeanna', 'filipeanna@gmail.com', 'filipeanna123', 0),
+(13, 'gui da cu', 'gui da cu', 'guidacu@gmail.com', '997754890', 0),
+(14, 'karen', 'karen beatriz', 'karenbeatrizrodriguessantana@gmail.com', 'KareN@123', 0),
+(15, 'dudao', 'dudao', 'mateusoliiveira000@gmail.com', '44444', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -100,13 +102,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `perguntas`
 --
 ALTER TABLE `perguntas`
-  MODIFY `id_perguntas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_perguntas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
